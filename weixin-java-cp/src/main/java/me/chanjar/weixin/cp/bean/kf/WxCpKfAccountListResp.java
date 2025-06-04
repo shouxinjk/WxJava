617168290f13a12b0ef5537e089ca390a22745f3
wang <2506904892@qq.com>
2025-06-04 15:56:36 +0800

    customer service add manage_privilege field

--- weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/kf/WxCpKfAccountListResp.java
+++ weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/kf/WxCpKfAccountListResp.java
@@ -50,6 +50,12 @@ public class WxCpKfAccountListResp extends WxCpBaseResp {
      */
     @SerializedName("avatar")
     private String avatar;
+
+    /**
+     * 当前调用接口的应用身份，是否有该客服账号的管理权限
+     */
+    @SerializedName("manage_privilege")
+    private Boolean managePrivilege;
   }
 
   /**
